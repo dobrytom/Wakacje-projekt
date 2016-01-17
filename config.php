26c5bc098d6e41d6eb078ae13ca57bc9514095b6
dobrytom <dobrytom@tlen.pl>
2016-01-17 01:18:55 +0100

    Revert "Wakacje"
    This reverts commit 1d0cdc6335e420fdc25fc6b7a3488fb3079c4ea6.

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-$host="mysql.keed.pl";	//adres serwera bazy danych
-$user="u869260137_tours";	//Nazwa użytkownika bazy danych
-$password="tm192168";	//Hasło logowania do bazy danych
-$database="u869260137_tours";	//Nazwa bazy danych; domyślnie "tours"
-$mail="dobrytom@tlen.pl"	//Adres e-mail do administratora
-?>
